b980760f40062b1eba61c9e063dcc094bf5a38d1
Daniel Fartanov <danil.fartanov@ticket.io>
2024-09-27 20:58:51 +0200

    feat: create DRO interfaces for Products

--- src/application/modules/product/dto/create-product.dto.ts
+++ src/application/modules/product/dto/create-product.dto.ts
@@ -1,1 +1,32 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from '@nestjs/class-validator';
+
+export default class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 78)
+  title: string;
+
+  @IsNotEmpty()
+  @IsNumber({ allowNaN: false, allowInfinity: false, maxDecimalPlaces: 2 })
+  @Min(1)
+  @Max(100000)
+  @IsPositive()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+}
--- src/application/modules/product/dto/update-product.dto.ts
+++ src/application/modules/product/dto/update-product.dto.ts
@@ -1,4 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from '@nestjs/class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export default class UpdateProductDto {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 78)
+  title?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsNumber({ allowNaN: false, allowInfinity: false, maxDecimalPlaces: 2 })
+  @Min(1)
+  @Max(100000)
+  @IsPositive()
+  price?: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+}
