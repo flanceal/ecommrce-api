c309ec3ee621887b74aca56031f06f1d9aeb942a
Daniel Fartanov <danil.fartanov@ticket.io>
2024-10-13 23:08:03 +0200

    feat: create a cart model in schema

---
+++ prisma/migrations/20241013191825_add_cart_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `Cart` (
+    `id` VARCHAR(36) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CartProduct` (
+    `cartId` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191) NOT NULL,
+    `quatity` INTEGER NOT NULL,
+
+    PRIMARY KEY (`cartId`, `productId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `CartProduct` ADD CONSTRAINT `CartProduct_cartId_fkey` FOREIGN KEY (`cartId`) REFERENCES `Cart`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CartProduct` ADD CONSTRAINT `CartProduct_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
